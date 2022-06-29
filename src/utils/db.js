1c4fa7fa9dc0b8b96efd352e1cd6722d70358001
charly abraham <charly@core.ai>
2022-06-29 16:31:02 +0530

    feat: added support for table creation

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,68 @@
 import {getMySqlConfigs} from "./configs.js";
 import mysql from "mysql2";
+import {isString} from "@aicore/libcommonutils";
 
-const client = mysql.createConnection(getMySqlConfigs());
+const CONNECTION = mysql.createConnection(getMySqlConfigs());
+// https://dev.mysql.com/doc/refman/8.0/en/identifier-length.html
+const MAXIMUM_LENGTH_OF_MYSQL_TABLE_NAME_AND_COLUMN_NAME = 64;
+const SIZE_OF_PRIMARY_KEY = 255;
 
-export function getValueForKey(key, table) {
+function _isValidTableAttributes(tableAttributeName) {
+    return (isString(tableAttributeName) && tableAttributeName.length <
+        MAXIMUM_LENGTH_OF_MYSQL_TABLE_NAME_AND_COLUMN_NAME);
+}
+
+export function createTable(tableName, nameOfPrimaryKey, nameOfJsonColumn) {
     return new Promise((resolve, reject) => {
-        client.query(`SELECT * FROM ${table} WHERE key = ?`, [key], (err, results, fields) => {
+        if (!_isValidTableAttributes(tableName)) {
+            reject('please provide valid table name');
+        }
+        if (!_isValidTableAttributes(nameOfPrimaryKey)) {
+            reject('please provide valid name for primary key');
+        }
+        if (!_isValidTableAttributes(nameOfJsonColumn)) {
+            reject('please provide valid name for json column');
+        }
+        try {
+            const createTableSql = `CREATE TABLE ${tableName} 
+                                        (${nameOfPrimaryKey} VARCHAR(${SIZE_OF_PRIMARY_KEY}) NOT NULL PRIMARY KEY, 
+                                        ${nameOfJsonColumn} JSON NOT NULL)`;
+            CONNECTION.execute(createTableSql,
+                function (err, results, fields) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve({
+                        results: results,
+                        fields: fields
+                    });
+                });
+        } catch (e) {
+            const errorMessage = `execution occurred while creating table ${e}`;
+            console.error(errorMessage);
+            reject(e);
+        }
+    });
+}
+
+/*
+export function getValueForKey(key, column, table) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT ${column} FROM ${table} WHERE ${key.column} = '${key.value}'`;
+        console.log(query);
+        client.query(query, (err, results, fields) => {
             if (err) {
                 reject(err);
             }
-            resolve(results[0].value);
+            resolve(results && results.length > 0 ? results[0] : null);
         });
     });
 }
 
-export function createTable(table, key, value) {
+export function createTable(table, keyColumn, valueColumn) {
     return new Promise((resolve, reject) => {
-        client.query(`CREATE TABLE ${table} (${key} VARCHAR(255) NOT NULL, ${value} JSON NOT NULL, PRIMARY KEY (${key}))`, (err, results, fields) => {
+        client.query(`CREATE TABLE ${table} (${keyColumn} VARCHAR(255) NOT NULL, ${valueColumn} JSON NOT NULL, PRIMARY KEY (${keyColumn}))`, (err, results, fields) => {
             if (err) {
                 reject(err);
             }
@@ -24,3 +70,16 @@ export function createTable(table, key, value) {
         });
     });
 }
+
+export function putValueForKey(key, value, table) {
+    return new Promise((resolve, reject) => {
+        client.query
+        client.query(`INSERT INTO ${table} (${key.column}, ${value.column}) VALUES ('${key.value}', '${JSON.stringify(value.value)}') ON DUPLICATE KEY UPDATE ${value.column} =  '${JSON.stringify(value.value)}'`, [key, value], (err, results, fields) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(results);
+        });
+    });
+}
+*/
