94cc5f47ebc30c8d6d5d310f9addb9e80c440e36
charly abraham <charly@core.ai>
2022-07-14 14:40:48 +0530

    fix: random string genreation with crypto

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,4 +1,3 @@
-import crypto from 'crypto';
 /**
  * This function helps to get configurations for mySQL DB
  *
@@ -6,9 +5,9 @@ import crypto from 'crypto';
 export function getMySqlConfigs() {
     const host = process.env.MY_SQL_SERVER || 'localhost';
     const port = process.env.MY_SQL_SERVER_PORT || '3306';
-    const database = process.env.MY_SQL_SERVER_DB || crypto.randomBytes(8).toString('hex');
-    const user = process.env.MY_SQL_USER || crypto.randomBytes(8).toString('hex');
-    const password = process.env.MY_SQL_PASSWORD || crypto.randomBytes(8).toString('hex');
+    const database = process.env.MY_SQL_SERVER_DB || generateRandomString(8);
+    const user = process.env.MY_SQL_USER || generateRandomString(8);
+    const password = process.env.MY_SQL_PASSWORD || generateRandomString(8);
     return {
         'host': host,
         'port': port,
@@ -16,4 +15,14 @@ export function getMySqlConfigs() {
         'user': user,
         'password': password
     };
+}
+
+// Create a random String for database configurations
+function generateRandomString(myLength) {
+    const chars =
+        "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    const randomArray = Array.from(
+        {length: myLength},
+        (v, k) => chars[Math.floor(Math.random() * chars.length)]
+    );
 }
