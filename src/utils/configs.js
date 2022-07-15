5c3518bb678aea6285ad68b3fad8400e7c8b6191
charly abraham <charly@core.ai>
2022-07-15 12:16:34 +0530

    fix: default size of db configs

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -7,9 +7,9 @@ import * as crypto from "crypto";
 export function getMySqlConfigs() {
     const host = process.env.MY_SQL_SERVER || 'localhost';
     const port = process.env.MY_SQL_SERVER_PORT || '3306';
-    const database = process.env.MY_SQL_SERVER_DB || 'a' + crypto.randomBytes(8).toString('hex');
-    const user = process.env.MY_SQL_USER || 'b' + crypto.randomBytes(8).toString('hex');
-    const password = process.env.MY_SQL_PASSWORD || 'c' + crypto.randomBytes(8).toString('hex');
+    const database = process.env.MY_SQL_SERVER_DB || 'a' + crypto.randomBytes(4).toString('hex');
+    const user = process.env.MY_SQL_USER || 'b' + crypto.randomBytes(4).toString('hex');
+    const password = process.env.MY_SQL_PASSWORD || 'c' + crypto.randomBytes(4).toString('hex');
     return {
         'host': host,
         'port': port,
