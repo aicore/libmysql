523c3292784ed88921c96d78b0e9f5cad220fc5d
charly abraham <charly@core.ai>
2022-07-17 18:12:44 +0530

    fix: config initialization

--- test/integration/setupIntegTest.js
+++ test/integration/setupIntegTest.js
@@ -1,13 +1,20 @@
 import {setUpMySQL} from '@aicore/libtestutils';
 
-const config = setUpMySQL();
-process.env.MY_SQL_SERVER = config.host;
-process.env.MY_SQL_SERVER_PORT = config.port;
-process.env.MY_SQL_SERVER_DB = config.database;
-process.env.MY_SQL_USER = config.user;
-process.env.MY_SQL_PASSWORD = config.password;
-console.log(`${config}`);
+let config = null;
+(async function () {
+    config = await setUpMySQL();
+    process.env.MY_SQL_SERVER = config.host;
+    process.env.MY_SQL_SERVER_PORT = config.port;
+    process.env.MY_SQL_SERVER_DB = config.database;
+    process.env.MY_SQL_USER = config.user;
+    process.env.MY_SQL_PASSWORD = config.password;
+    console.log(`${config}`);
 
-export function getMySqlConfigs(){
+}());
+
+export function getMySqlConfigs() {
+    if (!config) {
+        throw  new Error('config not initialized');
+    }
     return config;
 }
