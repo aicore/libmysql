283ca818ad4e4cabb20461575dd274dd6b47218c
charly abraham <charly@core.ai>
2022-07-17 16:37:58 +0530

    feat: creat table

---
+++ test/integration/setupIntegTest.js
@@ -0,0 +1,12 @@
+import {setUpMySQL} from '@aicore/libtestutils';
+
+const config = setUpMySQL();
+process.env.MY_SQL_SERVER = config.host;
+process.env.MY_SQL_SERVER_PORT = config.port;
+process.env.MY_SQL_SERVER_DB = config.database;
+process.env.MY_SQL_USER = config.user;
+process.env.MY_SQL_PASSWORD = config.password;
+
+export function getMySqlConfigs(){
+    return config;
+}
